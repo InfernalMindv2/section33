9d7824e20ad1fd75e043ab1fec2b42b3680981ce
InfernalMindv2 <147970105+InfernalMindv2@users.noreply.github.com>
2023-10-15 10:31:22 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double x,y,res=0;
+    cout << "Hello,please enter 2 numbers : ";
+    cin >> x >> y;
+    res = x + y;
+    cout << res;
+    return 0;
+}
